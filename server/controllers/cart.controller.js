18d6e82d08da6a38cedf7ba7c5dc3214879273ba
Andry Ariadi <andryariadi23@gmail.com>
2024-09-20 06:30:19 +0700

    handle addToCart removeAllCart updateQuantity and getCart product

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,18 +1,92 @@
+import Product from "../models/product.model.js";
+
 class Controller {
   static async getCartProducts(req, res) {
-    res.send("cart nih boss");
+    const user = req.user;
+
+    try {
+      const products = await Product.find({ _id: { $in: user.cartItems } });
+
+      const cartItems = products.map((product) => {
+        const cartItem = user.cartItems.find((item) => item.id === product._id);
+
+        return { ...product.toJSON(), quantity: cartItem.quantity };
+      });
+
+      res.status(200).json(cartItems);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error!", error: error.message });
+    }
   }
 
   static async addToCart(req, res) {
-    res.send("add to cart nih boss");
+    const { productId } = req.body;
+    const user = req.user;
+
+    try {
+      const existingItem = user.Items.find((item) => item.id === productId);
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        user.cartItmes.push(productId);
+      }
+
+      await user.save();
+
+      res.status(200).json({ message: "Product added to cart successfully!" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error!", error: error.message });
+    }
   }
 
   static async removeAllFromCart(req, res) {
-    res.send("remove from cart nih boss");
+    const { productId } = req.body;
+    const user = req.user;
+
+    try {
+      if (!productId) {
+        user.cartItems = [];
+      } else {
+        user.cartItems = user.cartItems.filter((item) => item !== productId);
+      }
+
+      await user.save();
+
+      res.status(200).json(user.cartItems, { message: "Product removed from cart successfully!" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error!", error: error.message });
+    }
   }
 
   static async updateQuantity(req, res) {
-    res.send("update quantity nih boss");
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+
+    try {
+      const existingItem = user.Items.find((item) => item.id === productId);
+
+      if (existingItem) {
+        if (quantity === 0) {
+          user.Items = user.Items.filter((item) => item.id !== productId);
+          await user.save();
+          return res.json(user.Items, { message: "Product removed from cart successfully!" });
+        }
+
+        existingItem.quantity = quantity;
+        await user.save();
+        return res.json(user.Items, { message: "Product quantity updated successfully!" });
+      } else {
+        return res.status(404).json({ message: "Product not found in cart!" });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error!", error: error.message });
+    }
   }
 }
 
