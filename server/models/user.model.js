4be5c0c84735a5278dabaab0167b7bbd3b7f7f48
Andry Ariadi <andryariadi23@gmail.com>
2024-09-19 07:08:53 +0700

    hash and compare password

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -33,11 +34,32 @@ const userSchema = new mongoose.Schema(
         },
       },
     ],
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.models?.User || mongoose.model("User", userSchema);
 
