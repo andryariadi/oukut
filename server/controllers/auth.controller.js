d1cfaf2d6fd843297d83b259a01a34d0cab49862
Andry Ariadi <andryariadi23@gmail.com>
2024-09-19 12:47:19 +0700

    handle login and logout

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateTokens } from "../libs/generateTokens.js";
 import { redis } from "../libs/redis.js";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 const storeRefreshToken = async (userId, refreshToken) => {
   await redis.set(`refreshToken:${userId}`, refreshToken, "EX", 7 * 60 * 60 * 24);
@@ -16,7 +17,7 @@ class Controller {
 
       const user = await User.create({ username, email, password });
 
-      const { accessToken, refreshToken } = generateTokens(user._id, res);
+      const { refreshToken } = generateTokens(user._id, res);
 
       await storeRefreshToken(user._id, refreshToken);
 
@@ -36,11 +37,51 @@ class Controller {
   }
 
   static async login(req, res) {
-    res.send("login api");
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+
+      if (user && (await user.comparePassword(password))) {
+        const { refreshToken } = generateTokens(user._id, res);
+
+        await storeRefreshToken(user._id, refreshToken);
+
+        res.status(200).json({
+          user: {
+            id: user?._id,
+            username: user?.username,
+            email: user?.email,
+            role: user?.role,
+          },
+          message: "Logged in successfully!",
+        });
+      } else {
+        res.status(401).json({ error: "Invalid email or password!" });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    }
   }
 
   static async logout(req, res) {
-    res.send("logout api");
+    try {
+      const refreshToken = req.cookies?.refreshToken;
+      if (refreshToken) {
+        const decode = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+        await redis.del(`refreshToken:${decode.userId}`);
+      }
+
+      res.clearCookie("refreshToken");
+      res.clearCookie("accessToken");
+
+      res.status(200).json({ message: "Logged out successfully!" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error!", error: error.message });
+    }
   }
 }
 
