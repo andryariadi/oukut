3d1d2144399f6a9e90c56d9641170f57439ec35a
Andry Ariadi <andryariadi23@gmail.com>
2024-09-21 15:51:38 +0700

    handle get and validate coupon

--- server/controllers/coupon.controller.js
+++ server/controllers/coupon.controller.js
@@ -1,6 +1,43 @@
+import Coupon from "../models/coupon.model.js";
+
 class Controller {
-  static async getCoupons(req, res) {
-    res.send("Get all coupons");
+  static async getCoupon(req, res) {
+    const user = req.user;
+
+    try {
+      const coupon = await Coupon.findOne({ userId: user._id, isActive: true });
+
+      res.status(200).json(coupon || null);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error!", error: error.message });
+    }
+  }
+
+  static async validateCoupon(req, res) {
+    const { code } = req.body;
+    const user = req.user;
+
+    try {
+      const coupon = await Coupon.findOne({ code, userId: user._id, isActive: true });
+
+      if (!coupon) return res.status(404).json({ message: "Coupon not found!" });
+
+      if (coupon.expirationDate < new Date()) {
+        coupon.isActive = false;
+        await coupon.save();
+        return res.status(400).json({ message: "Coupon expired!" });
+      }
+
+      res.status(200).json({
+        message: "Coupon is valid!",
+        code: coupon.code,
+        discountPercentage: coupon.discountPercentage,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error!", error: error.message });
+    }
   }
 }
 
--- server/routes/coupon.route.js
+++ server/routes/coupon.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import Controller from "../controllers/coupon.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", Controller.getCoupons);
+router.get("/", protectRoute, Controller.getCoupon);
+
+router.post("/validate", protectRoute, Controller.validateCoupon);
 
 export default router;
