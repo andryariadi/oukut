102ff4f05abd1855a14e15c3104aeb227848a15d
Andry Ariadi <andryariadi23@gmail.com>
2024-09-23 12:59:09 +0700

    handle get analtics data

--- server/routes/analytic.route.js
+++ server/routes/analytic.route.js
@@ -1,8 +1,26 @@
 import express from "express";
 import Controller from "../controllers/analytic.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const route = express.Router();
 
-route.get("/analytic-data", Controller.getAnalyticData);
+route.get("/", protectRoute, adminRoute, async (req, res) => {
+  try {
+    const analyticsData = await Controller.getAnalyticData();
+
+    const endDate = new Date();
+    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+    const dailySalesData = await Controller.getDailySalesData(startDate, endDate);
+
+    res.json({
+      analyticsData,
+      dailySalesData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error!", error: error.message });
+  }
+});
 
 export default route;
